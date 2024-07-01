7728d5daac82f90c98a9c2299af71095db056906
unknown <sandeeparaj93>
2024-07-01 23:44:42 +0530

    ServiceRequestProviderRepo

---
+++ ServiceRequestProvider/ServiceRequestManager/Constants/CommonConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace ServiceRequestManager.Constants
+{
+    // Current Status details as per the requiremet doc
+    public enum CurrentStatus
+    {
+        NotApplicable,
+        Created,
+        InProgress,
+        Complete,
+        Canceled
+    }
+    public static class ResponseStatus
+    {
+        public const string Successful = "Successful";
+        public const string UnSuccessful = "UnSuccessful";
+    }
+}
---
+++ ServiceRequestProvider/ServiceRequestManager/Context/ServiceRequestContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceRequestManager.Models;
+
+namespace ServiceRequestManager.Context
+{
+    // Context class 
+	public partial class ServiceRequestContext: DbContext
+	{
+        public ServiceRequestContext(DbContextOptions<ServiceRequestContext> options) : base(options) { }
+        public DbSet<ServiceRequestEntity> ServiceRequests { get; set; }
+    }
+}
